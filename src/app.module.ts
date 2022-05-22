d68ac41d5b4d1e1cc0d6684ecc7d241d38cbe952
Ariel <arielbrugi20@gmail.com>
2022-05-22 11:23:03 -0300

    feat(configuration): Se agrega biblioteca de mongose y nestconfig

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => {
+        return {
+          uri: configService.get('MONGO_URL'),
+        };
+      },
+    }),
+  ],
   controllers: [],
   providers: [],
 })
