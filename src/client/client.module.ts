81bf21e6c7a2c3ac6e43eb77136b5315a90c71f6
Ariel Brugiafredo <ariel.brugiafredo@darwoft.com>
2022-05-23 14:54:06 -0300

    feat(client): Se genera el schema del cliente con los metodos del reposotory

--- src/client/client.module.ts
+++ src/client/client.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { ClientService } from './client.service';
 import { ClientController } from './client.controller';
 import { ClientRepository } from './repository/client.repository';
+import {MongooseModule} from "@nestjs/mongoose";
+import {Client, ClientSchema} from "./schema/client.schema";
 
 @Module({
+  imports:[MongooseModule.forFeature([{name:Client.name,schema:ClientSchema}])],
   controllers: [ClientController],
   providers: [ClientService, ClientRepository],
 })
